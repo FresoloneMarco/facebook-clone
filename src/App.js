7e7bbcf5ac5a53967c50176b46fed2f72767849a
FresoloneMarco <m.fresolone2@studenti.unisa.it>
2020-09-26 11:35:36 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+    
+      <Header />
+      {/* AppBody */}
+        {/* SideBar */}
+        {/* Feed */}
+        {/* Widgets */}
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyChXcJKXZ7SmeEK-il-yAtVZGBj2KnCS7I",
+    authDomain: "facebook-clone-b9443.firebaseapp.com",
+    databaseURL: "https://facebook-clone-b9443.firebaseio.com",
+    projectId: "facebook-clone-b9443",
+    storageBucket: "facebook-clone-b9443.appspot.com",
+    messagingSenderId: "431808556135",
+    appId: "1:431808556135:web:460530809bec61b019b8c2",
+    measurementId: "G-CW1W0MQS9J"
+  };
