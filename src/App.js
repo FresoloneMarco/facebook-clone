fa40dc0349ecd26ba9b8dfaec612d1e7ef1fda1b
FresoloneMarco <m.fresolone2@studenti.unisa.it>
2020-09-26 17:58:11 +0200

    Starting DB part

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed'
 import Widgets from './Widgets'
+import Login from './Login'
 
 function App() {
+  const user = null;
   return (
     // BEM naming convention
     <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+         <Header />
 
-      <Header />
+         <div className='app__body'>
+           <Sidebar />
+           <Feed />
+           <Widgets />
+         </div>
+         </>
+      )}
 
-      <div className='app__body'>
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
+     
 
     </div>
 
