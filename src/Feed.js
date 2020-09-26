97d4ae0b009c30111a4d76ea8527de0184ad7028
FresoloneMarco <m.fresolone2@studenti.unisa.it>
2020-09-26 17:50:32 +0200

    Feeds Component Finished

--- src/Feed.js
+++ src/Feed.js
@@ -9,7 +9,14 @@ function Feed() {
         <div className='feed'>
             <StoryReel />
             <MessageSender />
-            <Post />
+            <Post
+                
+                profilePic='https://media-exp1.licdn.com/dms/image/C4D03AQGjvTYmUKLGPw/profile-displayphoto-shrink_200_200/0?e=1603929600&v=beta&t=TB3q8hzXcvN_dOaH-7Vf3c4cCQHEKNWz_KXwkBDsbZY'
+                message='Wow this works'
+                timestamp='this is a timestamp'
+                username='marcos'
+                image='https://s27389.pcdn.co/wp-content/uploads/2017/04/AdobeStock_112185177-3-1024x683.jpeg'
+/>
             <Post />
 
             <Post />
