2865b62709a3419ab7b798b46cc460a00e645477
FresoloneMarco <m.fresolone2@studenti.unisa.it>
2020-09-28 00:42:10 +0200

    Login Phase and React Context API

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,20 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
-    apiKey: "AIzaSyChXcJKXZ7SmeEK-il-yAtVZGBj2KnCS7I",
-    authDomain: "facebook-clone-b9443.firebaseapp.com",
-    databaseURL: "https://facebook-clone-b9443.firebaseio.com",
-    projectId: "facebook-clone-b9443",
-    storageBucket: "facebook-clone-b9443.appspot.com",
-    messagingSenderId: "431808556135",
-    appId: "1:431808556135:web:460530809bec61b019b8c2",
-    measurementId: "G-CW1W0MQS9J"
-  };
+  apiKey: "AIzaSyChXcJKXZ7SmeEK-il-yAtVZGBj2KnCS7I",
+  authDomain: "facebook-clone-b9443.firebaseapp.com",
+  databaseURL: "https://facebook-clone-b9443.firebaseio.com",
+  projectId: "facebook-clone-b9443",
+  storageBucket: "facebook-clone-b9443.appspot.com",
+  messagingSenderId: "431808556135",
+  appId: "1:431808556135:web:460530809bec61b019b8c2",
+  measurementId: "G-CW1W0MQS9J"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth, provider};
+export default db;
